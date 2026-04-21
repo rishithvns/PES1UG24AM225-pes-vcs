407deb214cf2334f9189d6537afe77af7c4a965d
Rishit <rishithvns15003@gmail.com>
2026-04-21 07:17:56 +0000

    Phase 3: all tests passing

--- index.c
+++ index.c
@@ -14,7 +14,7 @@
 //
 // PROVIDED functions: index_find, index_remove, index_status
 // TODO functions:     index_load, index_save, index_add
-/ 
+
 #include "index.h"
 #include <stdio.h>
 #include <stdlib.h>
